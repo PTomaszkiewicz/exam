0d4469bebcef1902f7c1d535596835adc200c120
PTomaszkiewicz <piotr.tomaszkiewicz@zs1mm.edu.pl>
2022-04-28 11:09:22 +0200

    nawet nie v0.2

---
+++ dbstructure.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Czas generowania: 28 Kwi 2022, 11:08
+-- Wersja serwera: 10.5.15-MariaDB-0ubuntu0.21.10.1
+-- Wersja PHP: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `mkw`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `images`
+--
+
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL,
+  `name` varchar(60) NOT NULL,
+  `picsum_id` int(11) NOT NULL,
+  `imagefile` varchar(70) NOT NULL,
+  `author` varchar(60) NOT NULL,
+  `width` int(11) NOT NULL,
+  `height` int(11) NOT NULL,
+  `added_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `images`
+--
+ALTER TABLE `images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- generate.php
+++ generate.php
@@ -1,5 +1,5 @@
 <?php 
-require_once 'vendor/autoload.php';
+require_once '../vendor/autoload.php';
 $servername = "localhost";
 $username = "susy";
 $password = "novell";
@@ -9,6 +9,31 @@ $db="mkw";
 $conn = new mysqli($servername, $username, $password, $db);
 $faker = Faker\Factory::create();
 for($i=0; $i<20; $i++){
-
+    $name=$faker->name();
+    $picsum_id=rand(0, 1000);
+    $imagefile="/sprawdzian/images/".str_replace(" ", "_", "$faker->name").".jpg";
+    $url="https://picsum.photos/id/$picsum_id/info";
+    $meta=file_get_contents($url);
+    if($meta==null){
+        return "Błąd";
+    } else {
+        $data= json_decode($meta); 
+        $author= $data->author;
+        $author= str_replace("`", " ", $author);
+        $author= str_replace("'", " ", $author);
+        $height= $data->height;
+        $width= $data->width;
+        $download= $data->download_url;
+        file_put_contents("./images/".$imagefile, file_get_contents($download));
+        $sql = "INSERT INTO images (name, picsum_id, imagefile, author, width, height)
+        VALUES ('$name', '$picsum_id', '$imagefile', '$author', '$width', '$height')";
+        
+        if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+        } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+        } 
+    }
+    
 }
 ?>
